789f58e9a34dae7186b51c06a1a2608709fc3bca
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-09 10:32:21 +0530

    Details page completed

--- disney-clone/src/components/Detail.js
+++ disney-clone/src/components/Detail.js
@@ -10,6 +10,29 @@ function Detail() {
       <ImageTitle>
         <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
       </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>Play</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>Trailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" />
+        </GroupWatchButton>
+      </Controls>
+      <SubTitle>2018 7m Family, Fantasy, Fiction</SubTitle>
+      <Description>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque magnam
+        eligendi porro voluptatibus maxime laudantium ea excepturi officia
+        praesentium iusto, vero fugit earum alias, assumenda facilis, autem
+        accusamus eaque quia.
+      </Description>
     </Container>
   );
 }
@@ -29,7 +52,7 @@ const Background = styled.div`
   left: 0;
   right: 0;
   z-index: -1;
-  opacity: 0.8;
+  opacity: 0.9;
   img {
     width: 100%;
     height: 100%;
@@ -48,4 +71,69 @@ const ImageTitle = styled.div`
     height: 100%;
     object-fit: contain;
   }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 0.4rem;
+  text-transform: uppercase;
+  cursor: pointer;
+  font-size: 2.4rem;
+  padding: 0 2.4rem;
+  margin-right: 2.2rem;
+  letter-spacing: 1.3px;
+  display: flex;
+  align-items: center;
+  height: 5.6rem;
+  background: rgb(249, 249, 249);
+  border: none;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+`;
+
+const AddButton = styled.button`
+  width: 4.4rem;
+  height: 4.4rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 1.6rem;
+  border-radius: 50%;
+  border 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  span {
+    font-size: 3rem;
+    color:white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  backfround: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 2.4rem;
+  min-height: 2rem;
+  margin-top: 2.6rem;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 2rem;
+  margin-top: 1.6rem;
+  color: rgb(249, 249, 249);
 `;
