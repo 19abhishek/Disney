646e6172c2b75654122bbc294cb145cba50f8d75
Abhishek Kumar <abhishek.kum1908@gmail.com>
2022-01-09 08:49:48 +0530

    Routing and movies section completed

--- disney-clone/src/components/Movies.js
+++ disney-clone/src/components/Movies.js
@@ -18,6 +18,18 @@ function Movies() {
         <Wrap>
           <img src="https://images.unsplash.com/photo-1622816691848-81646fbecbf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
         </Wrap>
+        <Wrap>
+          <img src="https://images.unsplash.com/photo-1622816691848-81646fbecbf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
+        </Wrap>
+        <Wrap>
+          <img src="https://images.unsplash.com/photo-1622816691848-81646fbecbf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
+        </Wrap>
+        <Wrap>
+          <img src="https://images.unsplash.com/photo-1622816691848-81646fbecbf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
+        </Wrap>
+        <Wrap>
+          <img src="https://images.unsplash.com/photo-1622816691848-81646fbecbf5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" />
+        </Wrap>
       </Content>
     </Container>
   );
@@ -39,12 +51,23 @@ const Content = styled.div`
 
 const Wrap = styled.div`
   border-radius: 1rem;
+  cursor: pointer;
   overflow: hidden;
   border: 0.3rem solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
 
+  transition: all 250ms;
   img {
     width: 100%;
     height: 100%;
     object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    border: color(249, 249, 249, 0.9);
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+      rgb(0 0 0 / 72%) 0px 30px 22px -10px;
   }
 `;
